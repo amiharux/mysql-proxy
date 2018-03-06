1fc07553e732065365a009b286cebc2546cec610
Mikhaylov, Alexander <Alexander.Mikhaylov@billing.ru>
2018-03-06 17:39:22 +0700

    inject callbacks on reading

--- mysql-proxy/main.cpp
+++ mysql-proxy/main.cpp
@@ -10,6 +10,8 @@
 
 #include "logger.h"
 
+using on_data_cb = std::function<void(unsigned char*, size_t)>;
+const on_data_cb dummy_cb = [](unsigned char*, size_t) {};
 
 class bridge : public std::enable_shared_from_this<bridge> {
 public:
@@ -17,9 +19,11 @@ public:
   using socket_type = asio::ip::tcp::socket;
   using ptr_type = std::shared_ptr<bridge>;
 
-  bridge(asio::io_service& ios)
+  bridge(asio::io_service& ios, on_data_cb on_client_data = dummy_cb, on_data_cb on_server_data = dummy_cb)
     : client_socket_(ios)
     , server_socket_(ios)
+    , on_client_data_(on_client_data)
+    , on_server_data_(on_server_data)
   {}
 
   socket_type& client_socket() {
@@ -37,7 +41,7 @@ public:
       ss << le.address().to_string() << "::" << le.port();
     } 
     catch (const std::system_error &err) {
-      ss << "<socket_closed:" << err.code() << ">";
+      ss << "<" << err.code() << ">";
     }
     return ss.str();
   }
@@ -78,6 +82,7 @@ private:
 
     server_socket_.async_read_some(asio::buffer(server_data_, max_data_length),
       [self = shared_from_this()](const asio::error_code& error, size_t bytes) {
+        self->on_server_data_(self->server_data_, bytes);
         self->async_write_to_client(error, bytes);
       }
     );
@@ -100,6 +105,7 @@ private:
 
     client_socket_.async_read_some(asio::buffer(client_data_, max_data_length),
       [self = shared_from_this()](const asio::error_code& error, size_t bytes) {
+        self->on_client_data_(self->client_data_, bytes);
         self->async_write_to_server(error, bytes);
       }
     );
@@ -126,27 +132,32 @@ private:
   unsigned char client_data_[max_data_length];
   unsigned char server_data_[max_data_length];
 
+  on_data_cb on_client_data_;
+  on_data_cb on_server_data_;
+
   std::mutex mutex_;
 
 public:
 
   class acceptor {
   public:
-
     acceptor(asio::io_service& io_service,
       const std::string& listen_host, unsigned short listen_port,
-      const std::string& server_host, unsigned short server_port)
+      const std::string& server_host, unsigned short server_port,
+      on_data_cb on_client_data = dummy_cb, on_data_cb on_server_data = dummy_cb)
       : io_service_(io_service)
       , listenhost_address(asio::ip::address_v4::from_string(listen_host))
       , acceptor_(io_service_, asio::ip::tcp::endpoint(listenhost_address, listen_port))
       , server_port_(server_port)
       , server_host_(server_host)
+      , on_client_data_(on_client_data)
+      , on_server_data_(on_server_data)
     {}
 
     bool accept_connections() {
       LOG << "bridge::acceptor::accept_connections";
       try {
-        session_ = std::make_shared<bridge>(io_service_);
+        session_ = std::make_shared<bridge>(io_service_, on_client_data_, on_server_data_);
 
         acceptor_.async_accept(session_->client_socket(),
           [&](const asio::error_code& error) {
@@ -182,6 +193,9 @@ public:
     ptr_type session_;
     unsigned short server_port_;
     std::string server_host_;
+
+    on_data_cb on_client_data_;
+    on_data_cb on_server_data_;
   };
 };
 
@@ -200,11 +214,20 @@ int main(int argc, char* argv[]) {
 
   asio::io_service ios;
 
+  auto on_client_data = [](unsigned char*, size_t bytes) {
+    LOG << "Got " << bytes << " bytes from client";
+  };
+
+  auto on_server_data = [](unsigned char*, size_t bytes) {
+    LOG << "Got " << bytes << " bytes from server";
+  };
+
   try {
     LOG << "starting server on " << listen_host << "::" << listen_port;
     bridge::acceptor acceptor(ios,
       listen_host, listen_port,
-      forward_host, forward_port);
+      forward_host, forward_port,
+      on_client_data, on_server_data);
 
     acceptor.accept_connections();
 
